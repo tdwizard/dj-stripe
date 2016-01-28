5b3f466ce3f6fc832186704cbfe2475732419cfc
Tomasz Drobiszewski <tdrobiszewski@tdwizard.com>
2016-01-28 13:38:51 +0100

    add account

--- djstripe/models.py
+++ djstripe/models.py
@@ -803,6 +803,9 @@ class Account(StripeObject):
         self.bank_account = stripe_bank.id
         self.save()
         return self
+
+    def transfer(self, amount, currency="usd"):
+
 # Much like registering signal handlers. We import this module so that its registrations get picked up
 # the NO QA directive tells flake8 to not complain about the unused import
 from . import event_handlers  # NOQA
